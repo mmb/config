c15e63f180c7e15aa09bd4c5f618f751ededb11a
Matthew M. Boedicker <matthewm@boedicker.org>
2010-02-23 16:59:39 -0500

    change from .emacs to .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,21 @@
+; turn off tool bar and menu bar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+(global-font-lock-mode 1)
+
+(require 'color-theme)
+(require 'color-theme-tango)
+(color-theme-tango)
+
+(require 'textile-mode)
+(add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(require 'xml-pretty)
+(global-set-key "\C-c\C-p" 'xml-pretty)
+
+(global-set-key "\C-c\C-s" 'sort-lines)
+
+(server-start)
---
+++ .emacs.d/xml-pretty.el
@@ -0,0 +1,5 @@
+(defun xml-pretty ()
+  "make current buffer pretty if in sgml mode"
+  (interactive)
+  (when (eq major-mode 'sgml-mode)
+    (sgml-pretty-print (point-min) (point-max))))
