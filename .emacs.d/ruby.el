6725ac0d7fc9cdfecb51dc92a0b6265e76dbdc5a
Matthew M. Boedicker <matthewm@boedicker.org>
2010-05-12 22:22:22 -0400

    use for dolist for ruby mode special cases

--- .emacs.d/ruby.el
+++ .emacs.d/ruby.el
@@ -1,1 +1,5 @@
-(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+; ruby files not ending in .rb that ruby mode should be used for
+(dolist (pattern (list
+		  "config\\.ru\\'"
+		  "Rakefile\\'"
+		  )) (add-to-list 'auto-mode-alist (cons pattern 'ruby-mode)))
