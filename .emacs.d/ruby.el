8442dfbb6acaf7f91663f1eac2cfca1c04b3c0f0
Matthew M. Boedicker <matthewm@boedicker.org>
2012-09-12 23:02:56 -0700

    Add capistrano files.

--- .emacs.d/ruby.el
+++ .emacs.d/ruby.el
@@ -1,5 +1,7 @@
 ; ruby files not ending in .rb that ruby mode should be used for
 (dolist (pattern (list
+		  "Capfile\\'"
+		  "capfile\\'"
 		  "config\\.ru\\'"
 		  "Rakefile\\'"
 		  )) (add-to-list 'auto-mode-alist (cons pattern 'ruby-mode)))
