ffb4b9334bc8f08308d6b395446c25175cb02569
Matthew M. Boedicker <matthewm@boedicker.org>
2012-11-17 22:51:31 -0800

    Don't indent with tabs in js-mode.

--- .emacs.d/javascript.el
+++ .emacs.d/javascript.el
@@ -1,1 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.json\\'" . js-mode))
+
+; Indent with spaces instead of tabs.
+(add-hook 'js-mode-hook 'my-disable-indent-tabs-mode)
+(defun my-disable-indent-tabs-mode ()
+  (set-variable 'indent-tabs-mode nil))
